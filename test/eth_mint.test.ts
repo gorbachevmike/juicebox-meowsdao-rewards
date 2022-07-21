e6f08f5336ebec6873bb0bfaae0ca3d3998e5a9b
cptspacecadet <m@dao-lawfirm.xyz>
2022-07-21 17:19:04 -0400

    - eth mint tests

--- test/eth_mint.test.ts
+++ test/eth_mint.test.ts
@@ -1,15 +1,26 @@
 import { expect } from 'chai';
 import { ethers } from 'hardhat';
+import fetch from 'node-fetch';
+
 import { deployMockContract } from '@ethereum-waffle/mock-contract';
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
 
 import jbDirectory from '../node_modules/@jbx-protocol/contracts-v2/deployments/mainnet/jbDirectory.json';
 import jbETHPaymentTerminal from '../node_modules/@jbx-protocol/contracts-v2/deployments/mainnet/jbETHPaymentTerminal.json';
 
+async function deployMockContractFromAddress(contractAddress: string, etherscanKey: string, account: any) {
+    const abi = await fetch(`https://api.etherscan.io/api?module=contract&action=getabi&address=${contractAddress}&apikey=${etherscanKey}`)
+        .then(response => response.json())
+        .then(data => JSON.parse(data['result']));
+
+    return deployMockContract(account, abi);
+}
+
 describe('MEOWs DAO Token Mint Tests', () => {
     const tokenUnitPrice = ethers.utils.parseEther('0.0125');
 
-    let deployer: any;
-    let accounts: any[];
+    let deployer: SignerWithAddress;
+    let accounts: SignerWithAddress[];
     let token: any;
 
     before(async () => {
@@ -18,14 +29,20 @@ describe('MEOWs DAO Token Mint Tests', () => {
         const tokenBaseUri = 'ipfs://hidden';
         const tokenContractUri = 'ipfs://metadata';
         const jbxProjectId = 99;
-        const tokenMaxSupply = 1000;
-        const tokenMintAllowance = 10;
+        const tokenMaxSupply = 8;
+        const tokenMintAllowance = 6;
 
         [deployer, ...accounts] = await ethers.getSigners();
 
+        const mockUniswapQuoter = await deployMockContractFromAddress('0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6', process.env.ETHERSCAN_KEY || '', deployer);
+        await mockUniswapQuoter.mock.quoteExactInputSingle.returns('1211000000000000000000');
+
+        const mockUniswapRouter = await deployMockContractFromAddress('0xE592427A0AEce92De3Edee1F18E0157C05861564', process.env.ETHERSCAN_KEY || '', deployer);
+
         const jbxJbTokensEth = '0x000000000000000000000000000000000000EEEe';
         const ethTerminal = await deployMockContract(deployer, jbETHPaymentTerminal.abi);
-        // TODO: mock token terminal
+        await ethTerminal.mock.addToBalanceOf.returns();
+        await ethTerminal.mock.pay.returns();
 
         const mockDirectory = await deployMockContract(deployer, jbDirectory.abi);
         await mockDirectory.mock.isTerminalOf.withArgs(jbxProjectId, ethTerminal.address).returns(true);
@@ -41,11 +58,64 @@ describe('MEOWs DAO Token Mint Tests', () => {
             mockDirectory.address,
             tokenMaxSupply,
             tokenUnitPrice,
-            tokenMintAllowance
+            tokenMintAllowance,
+            mockUniswapQuoter.address,
+            mockUniswapRouter.address
         );
     });
 
+    it('User mints first: fail due to price', async () => {
+        await expect(token.connect(accounts[0])['mint()']({value: tokenUnitPrice}))
+            .to.be.revertedWith('INCORRECT_PAYMENT(0)');
+    });
+
     it('User mints first', async () => {
+        await expect(token.connect(accounts[0])['mint()']({value: 0}))
+            .to.emit(token, 'Transfer');
+    });
+
+    it('User mints second: fail due to price', async () => {
+        await expect(token.connect(accounts[0])['mint()']({value: 0}))
+            .to.be.revertedWith('INCORRECT_PAYMENT(12500000000000000)');
+    });
+
+    it('User mints second', async () => {
+        await expect(token.connect(accounts[0])['mint()']({value: tokenUnitPrice}))
+            .to.emit(token, 'Transfer');
+    });
+
+    it('User mints third: fail due to price', async () => {
+        await expect(token.connect(accounts[0])['mint()']({value: tokenUnitPrice}))
+            .to.be.revertedWith('INCORRECT_PAYMENT(0)');
+    });
+
+    it('User mints third', async () => {
+        await expect(token.connect(accounts[0])['mint()']({value: 0}))
+            .to.emit(token, 'Transfer');
+    });
+
+    it('User mints 4-6', async () => {
+        await expect(token.connect(accounts[0])['mint()']({value: tokenUnitPrice.mul(3)})).to.emit(token, 'Transfer');
         await expect(token.connect(accounts[0])['mint()']({value: 0})).to.emit(token, 'Transfer');
+        await expect(token.connect(accounts[0])['mint()']({value: tokenUnitPrice.mul(5)})).to.emit(token, 'Transfer');
+
+        expect(await token.balanceOf(accounts[0].address)).to.equal(6);
+    });
+
+    it('User mints 7: allowance failure', async () => {
+        await expect(token.connect(accounts[0])['mint()']({value: 0}))
+        .to.be.revertedWith('ALLOWANCE_EXHAUSTED()');
+    });
+
+    it('Admin mints', async () => {
+        await expect(token.connect(deployer)['mintFor(address)'](accounts[1].address, {value: 0}))
+            .to.emit(token, 'Transfer');
+
+        expect(await token.balanceOf(accounts[1].address)).to.equal(1);
+    });
+
+    it('Another user mints the rest', async () => {
+        await expect(token.connect(accounts[1])['mint()']({value: tokenUnitPrice})).to.emit(token, 'Transfer');
+        await expect(token.connect(accounts[1])['mint()']({value: 0})).to.be.revertedWith('SUPPLY_EXHAUSTED()');
     });
 });
