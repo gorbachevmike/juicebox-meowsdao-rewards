e172d6a72048d2aa1776d93ffa756755b6a00762
cptspacecadet <m@dao-lawfirm.xyz>
2022-07-27 23:16:55 -0400

    - checkpoint

--- contracts/MeowGatewayUtil.sol
+++ contracts/MeowGatewayUtil.sol
@@ -3,8 +3,39 @@ pragma solidity 0.8.6;
 
 import '@openzeppelin/contracts/utils/Strings.sol';
 
+// stack[0] = uint64(uint8(_traits)); // Background 165
+// stack[1] = uint64(uint8(_traits >> 8) & 63); // Fur 25
+// stack[2] = uint64(uint8(_traits >> 14) & 15); // Ears 2
+// stack[3] = uint64(uint8(_traits >> 18) & 15); // Brows 2
+// stack[4] = uint64(uint8(_traits >> 22) & 63); // Eyes 36
+// stack[5] = uint64(uint8(_traits >> 30) & 15); // Nose 19
+
+// stack[6] = uint64(uint8(_traits >> 34) & 15); // Nipples 2 // naked
+// stack[6] = uint64(uint8(_traits >> 38) & 15); // Shirt 11 // shirt
+// stack[7] = uint64(uint8(_traits >> 42) & 15); // Tie 10 // shirt
+// stack[8] = uint64(uint8(_traits >> 46) & 15); // Blazer 7 // shirt
+// stack[6] = uint64(uint8(_traits >> 50) & 15); // T-shirt 13 // tshirt
+// stack[7] = uint64(uint8(_traits >> 54) & 63); // Pattern 34 // tshirt
+
+// stack[7] = uint64(uint8(_traits >> 60) & 63); // Headwear 31
+// stack[8] = uint64(uint8(_traits >> 66) & 63); // Glasses 37
+// stack[9] = uint64(uint8(_traits >> 72) & 15); // Collar 14 // naked
+// stack[10] = uint64(uint8(_traits >> 76) & 1); // Signature 1
+// stack[11] = uint64(uint8(_traits >> 77) & 1); // Juicebox 1
+
 contract MeowGatewayUtil {
-  function validateTraits(uint256 _traits) public pure returns (bool) {
+    uint8[12] private nakedOffsets = [0, 8, 14, 18, 22, 30, 34, 60, 66, 72, 76, 77];
+    uint8[12] private nakedCardinality = [165, 25, 2, 2, 36, 19, 2, 31, 37, 14, 1, 1];
+    uint8[12] private nakedMask = [255, 63, 15, 15, 63, 15, 15, 63, 63, 15, 1, 1];
+    uint8[12] private tShirtOffsets = [0, 8, 14, 18, 22, 30, 50, 54, 60, 66, 76, 77];
+    uint8[12] private tShirtCardinality = [165, 25, 2, 2, 37, 19, 13, 34, 31, 37, 1, 1];
+    uint8[12] private tShirtMask = [255, 63, 15, 15, 63, 15, 15, 63, 63, 63, 1, 1];
+    uint8[13] private shirtOffsets = [0, 8, 14, 18, 22, 30, 38, 42, 46, 60, 66, 76, 77];
+    uint8[13] private shirtCardinality = [165, 25, 2, 2, 36, 19, 11, 10, 7, 31, 37, 1, 1];
+    uint8[13] private shirtMask = [255, 63, 15, 15, 63, 15, 15, 15, 15, 63, 63, 1, 1];
+
+
+  function validateTraits(uint256 _traits) public view returns (bool) {
     uint8 population = uint8(_traits >> 252) & 15;
 
     if (population == 0) {
@@ -16,12 +47,9 @@ contract MeowGatewayUtil {
     }
   }
 
-  function validateNakedTraits(uint256 _traits) private pure returns (bool) {
-    uint8[10] memory offsets = [0, 4, 8, 12, 20, 28, 36, 40, 44, 52];
-    uint8[10] memory cardinality = [5, 5, 5, 17, 70, 18, 7, 3, 68, 35];
-
-    for (uint8 i = 0; i != 9; ) {
-      if (uint8(_traits >> offsets[i]) > cardinality[i]) {
+  function validateNakedTraits(uint256 _traits) private view returns (bool) {
+    for (uint8 i = 0; i != 12; ) {
+      if (uint8(_traits >> nakedOffsets[i]) & nakedMask[i] > nakedCardinality[i]) {
         return false;
       }
       ++i;
@@ -30,12 +58,9 @@ contract MeowGatewayUtil {
     return true;
   }
 
-  function validateTShirtTraits(uint256 _traits) private pure returns (bool) {
-    uint8[10] memory offsets = [0, 4, 8, 12, 20, 28, 36, 40, 44, 52];
-    uint8[10] memory cardinality = [5, 5, 5, 17, 70, 18, 7, 3, 68, 35];
-
-    for (uint8 i = 0; i != 9; ) {
-      if (uint8(_traits >> offsets[i]) > cardinality[i]) {
+  function validateTShirtTraits(uint256 _traits) private view returns (bool) {
+    for (uint8 i = 0; i != 12; ) {
+      if (uint8(_traits >> tShirtOffsets[i]) & tShirtMask[i] > tShirtCardinality[i]) {
         return false;
       }
       ++i;
@@ -44,12 +69,9 @@ contract MeowGatewayUtil {
     return true;
   }
 
-  function validateShirtTraits(uint256 _traits) private pure returns (bool) {
-    uint8[10] memory offsets = [0, 4, 8, 12, 20, 28, 36, 40, 44, 52];
-    uint8[10] memory cardinality = [5, 5, 5, 17, 70, 18, 7, 3, 68, 35];
-
-    for (uint8 i = 0; i != 9; ) {
-      if (uint8(_traits >> offsets[i]) > cardinality[i]) {
+  function validateShirtTraits(uint256 _traits) private view returns (bool) {
+    for (uint8 i = 0; i != 13; ) {
+      if (uint8(_traits >> shirtOffsets[i]) & shirtMask[i] > shirtCardinality[i]) {
         return false;
       }
       ++i;
@@ -58,13 +80,38 @@ contract MeowGatewayUtil {
     return true;
   }
 
-  function generateTraits(uint256 _seed) public pure returns (uint256 traits) {
-    uint8[10] memory offsets = [0, 4, 8, 12, 20, 28, 36, 40, 44, 52];
-    uint8[10] memory cardinality = [5, 5, 5, 17, 70, 18, 7, 3, 68, 35];
+  function generateTraits(uint256 _seed) public view returns (uint256 traits) {
+    uint8 population = uint8(_seed >> 252) & 15;
 
-    traits = uint256(uint8(_seed) % cardinality[0]);
-    for (uint8 i = 1; i != 9; ) {
-      traits |= uint256((uint8(_seed >> offsets[i]) % cardinality[i])) << offsets[i];
+    if (population == 0) {
+      return generateNakedTraits(_seed);
+    } else if (population == 1) {
+      return generateTShirtTraits(_seed);
+    } else {
+      return generateShirtTraits(_seed);
+    }
+  }
+
+  function generateNakedTraits(uint256 _seed) private view returns (uint256 traits) {
+    traits = uint256(uint8(_seed) % nakedCardinality[0]);
+    for (uint8 i = 1; i != 12; ) {
+      traits |= uint256((uint8(_seed >> nakedOffsets[i]) % nakedCardinality[i])) << nakedOffsets[i];
+      ++i;
+    }
+  }
+
+  function generateTShirtTraits(uint256 _seed) private view returns (uint256 traits) {
+    traits = uint256(uint8(_seed) % tShirtCardinality[0]);
+    for (uint8 i = 1; i != 12; ) {
+      traits |= uint256((uint8(_seed >> tShirtOffsets[i]) % tShirtCardinality[i])) << tShirtOffsets[i];
+      ++i;
+    }
+  }
+
+  function generateShirtTraits(uint256 _seed) private view returns (uint256 traits) {
+    traits = uint256(uint8(_seed) % shirtCardinality[0]);
+    for (uint8 i = 1; i != 13; ) {
+      traits |= uint256((uint8(_seed >> shirtOffsets[i]) % shirtCardinality[i])) << shirtOffsets[i];
       ++i;
     }
   }
@@ -99,64 +146,10 @@ contract MeowGatewayUtil {
     string calldata _ipfsRoot,
     uint256 _traits
   ) internal view returns (string memory image) {
-    // stack[0] = uint64(uint8(_traits)); // Background
-    // stack[1] = uint64(uint8(_traits >> 8) & 63); // Fur
-    // stack[2] = uint64(uint8(_traits >> 14) & 15); // Ears
-    // stack[3] = uint64(uint8(_traits >> 18) & 15); // Brows
-    // stack[4] = uint64(uint8(_traits >> 22)); // Eyes
-    // stack[5] = uint64(uint8(_traits >> 30) & 15); // Nose
-    // stack[6] = uint64(uint8(_traits >> 34) & 15); // Nipples
-    // stack[7] = uint64(uint8(_traits >> 38) & 63); // Headwear
-    // stack[8] = uint64(uint8(_traits >> 44) & 63); // Glasses
-    // stack[9] = uint64(uint8(_traits >> 50) & 15); // Collar
-    // stack[10] = uint64(uint8(_traits >> 54) & 1); // Signature
-    // stack[11] = uint64(uint8(_traits >> 55) & 1); // Juicebox
-
-    uint8[12] memory offsets = [0, 8, 14, 18, 22, 30, 34, 38, 44, 50, 54, 55];
-    uint64[12] memory stack;
-    for (uint8 i; i < 12; ) {
-      uint8 mask = i == 127 ? 0 : uint8(2**(offsets[i] - offsets[i - 1]) - 1);
-      stack[i] = uint64(uint8(_traits >> offsets[i]) & mask);
-      ++i;
-    }
-
-    image = __imageTag(_ipfsGateway, _ipfsRoot, stack[0]);
+    image = __imageTag(_ipfsGateway, _ipfsRoot, uint64(uint8(_traits >> nakedOffsets[0]) & nakedMask[0]));
     for (uint8 i = 1; i < 12; ) {
-      image = string(abi.encodePacked(image, __imageTag(_ipfsGateway, _ipfsRoot, stack[i])));
-      ++i;
-    }
-  }
-
-  function getShirtStack(
-    string calldata _ipfsGateway,
-    string calldata _ipfsRoot,
-    uint256 _traits
-  ) internal view returns (string memory image) {
-    // stack[0] = uint64(uint8(_traits)); // Background
-    // stack[1] = uint64(uint8(_traits >> 8) & 63); // Fur
-    // stack[2] = uint64(uint8(_traits >> 14) & 15); // Ears
-    // stack[3] = uint64(uint8(_traits >> 18) & 15); // Brows
-    // stack[4] = uint64(uint8(_traits >> 22)); // Eyes
-    // stack[5] = uint64(uint8(_traits >> 30) & 15); // Nose
-    // stack[6] = uint64(uint8(_traits >> 34) & 15); // Shirt
-    // stack[7] = uint64(uint8(_traits >> 38) & 15); // Tie
-    // stack[8] = uint64(uint8(_traits >> 42) & 15); // Blazer
-    // stack[9] = uint64(uint8(_traits >> 46) & 63); // Headwear
-    // stack[10] = uint64(uint8(_traits >> 52) & 63); // Glasses
-    // stack[11] = uint64(uint8(_traits >> 58) & 1); // Signature
-    // stack[12] = uint64(uint8(_traits >> 59) & 1); // Juicebox
-
-    uint8[13] memory offsets = [0, 8, 14, 18, 22, 30, 34, 38, 42, 46, 52, 58, 59];
-    uint64[13] memory stack;
-    for (uint8 i; i < 12; ) {
-      uint8 mask = i == 0 ? 127 : uint8(2**(offsets[i] - offsets[i - 1]) - 1);
-      stack[i] = uint64(uint8(_traits >> offsets[i]) & mask);
-      ++i;
-    }
-
-    image = __imageTag(_ipfsGateway, _ipfsRoot, stack[0]);
-    for (uint8 i = 1; i < 12; ) {
-      image = string(abi.encodePacked(image, __imageTag(_ipfsGateway, _ipfsRoot, stack[i])));
+      image = string(
+        abi.encodePacked(image, __imageTag(_ipfsGateway, _ipfsRoot, uint64(uint8(_traits >> nakedOffsets[i]) & nakedMask[i]))));
       ++i;
     }
   }
@@ -166,30 +159,23 @@ contract MeowGatewayUtil {
     string calldata _ipfsRoot,
     uint256 _traits
   ) internal view returns (string memory image) {
-    // stack[0] = uint64(uint8(_traits)); // Background
-    // stack[1] = uint64(uint8(_traits >> 8) & 63); // Fur
-    // stack[2] = uint64(uint8(_traits >> 14) & 15); // Ears
-    // stack[3] = uint64(uint8(_traits >> 18) & 15); // Brows
-    // stack[4] = uint64(uint8(_traits >> 22)); // Eyes
-    // stack[5] = uint64(uint8(_traits >> 30) & 15); // Nose
-    // stack[6] = uint64(uint8(_traits >> 34) & 15); // T-shirt
-    // stack[7] = uint64(uint8(_traits >> 38) & 63); // Pattern
-    // stack[8] = uint64(uint8(_traits >> 44) & 63); // Headwear
-    // stack[9] = uint64(uint8(_traits >> 50) & 63); // Glasses
-    // stack[10] = uint64(uint8(_traits >> 56) & 1); // Signature
-    // stack[11] = uint64(uint8(_traits >> 57) & 1); // Juicebox
-
-    uint8[12] memory offsets = [0, 8, 14, 18, 22, 30, 34, 38, 44, 50, 56, 57];
-    uint64[12] memory stack;
-    for (uint8 i; i < 12; ) {
-      uint8 mask = i == 0 ? 127 : uint8(2**(offsets[i] - offsets[i - 1]) - 1);
-      stack[i] = uint64(uint8(_traits >> offsets[i]) & mask);
+    image = __imageTag(_ipfsGateway, _ipfsRoot, uint64(uint8(_traits >> tShirtOffsets[0]) & tShirtMask[0]));
+    for (uint8 i = 1; i < 12; ) {
+      image = string(
+        abi.encodePacked(image, __imageTag(_ipfsGateway, _ipfsRoot, uint64(uint8(_traits >> tShirtOffsets[i]) & tShirtMask[i]))));
       ++i;
     }
+  }
 
-    image = __imageTag(_ipfsGateway, _ipfsRoot, stack[0]);
-    for (uint8 i = 1; i < 12; ) {
-      image = string(abi.encodePacked(image, __imageTag(_ipfsGateway, _ipfsRoot, stack[i])));
+  function getShirtStack(
+    string calldata _ipfsGateway,
+    string calldata _ipfsRoot,
+    uint256 _traits
+  ) internal view returns (string memory image) {
+    image = __imageTag(_ipfsGateway, _ipfsRoot, uint64(uint8(_traits >> shirtOffsets[0]) & shirtMask[0]));
+    for (uint8 i = 1; i < 13; ) {
+      image = string(
+        abi.encodePacked(image, __imageTag(_ipfsGateway, _ipfsRoot, uint64(uint8(_traits >> shirtOffsets[i]) & shirtMask[i]))));
       ++i;
     }
   }
